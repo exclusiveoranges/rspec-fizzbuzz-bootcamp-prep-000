54b9bcce3eea631bd7bb6eb23af6bd75012c08e2
Ryan O'Toole <otoolerj@gmail.com>
2018-03-13 18:35:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 5 == 0 && int % 3 == 0
-    return "Fizz"
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
   elsif int % 3 == 0
-    return "FizzBuzz"
+    return "Fizz"
   else
     return nil
   end
